287360435763e8c1bc89d5bf0075210070998273
celerlynx <celerlynx@gmail.com>
2026-01-05 20:53:41 +0500

    fixing vertical sticking

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -18,17 +15,22 @@ public class Ball : MonoBehaviour
         
         //after a collision we accelerate a bit
         velocity += velocity.normalized * 0.01f;
-        
+       
         //check if we are not going totally vertically as this would lead to being stuck, we add a little vertical force
         if (Vector3.Dot(velocity.normalized, Vector3.up) < 0.1f)
         {
-            velocity += velocity.y > 0 ? Vector3.up * 0.5f : Vector3.down * 0.5f;
+            float delta = Random.Range(0.2f, 0.5f);
+            velocity += velocity.y > 0 ? Vector3.up * delta : Vector3.down * delta;
+        }
+        if (Mathf.Abs(velocity.x) <= 0.1f)
+        {
+            m_Rigidbody.AddForce(new Vector3(-5f, 0, 0));
         }
 
         //max velocity
         if (velocity.magnitude > 3.0f)
         {
-            velocity = velocity.normalized * 3.0f;
+            velocity = velocity.normalized * 2.5f;
         }
 
         m_Rigidbody.linearVelocity = velocity;
